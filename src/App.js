06f8f3a55670219133873f31102c52ab1fc8f0b9
omar <omarrowing92@gmail.com>
2021-01-21 17:43:45 +0100

    checkpoint react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from './component/NavBar';
+import  Forms from './component/Forms';
+import FooterPage from './component/FooterPage';
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Forms/>
+      <FooterPage/>
+    </div>
+  );
+}
+
+export default App;
